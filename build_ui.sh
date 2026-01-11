fc864e06d524e509d2b9b538c34beeeb4b17ed37
Viet The Le <letheviet0708@gmail.com>
2026-01-11 08:34:05 +0700

    feat: Ensure directories are created before copying build files in build_ui.sh

--- build_ui.sh
+++ build_ui.sh
@@ -8,10 +8,12 @@ pnpm build
 cd ..
 
 echo "Copying to VS Code Extension..."
+mkdir -p vscode-extension/media
 rm -rf vscode-extension/media/*
 cp -r web-ui/build/* vscode-extension/media/
 
 echo "Copying to Python Viewer..."
+mkdir -p langlens/viewer/static
 rm -rf langlens/viewer/static/*
 cp -r web-ui/build/* langlens/viewer/static/
 
